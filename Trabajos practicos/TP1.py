6077114f28d829e8527847c3a9ecf54de0c43d26
DaSilvaJoaquin <127798778+DaSilvaJoaquin@users.noreply.github.com>
2023-05-02 18:55:32 -0300

    Add files via upload

---
+++ Trabajos practicos/TP1.py
@@ -0,0 +1,31 @@
+#Ejercicio 5
+romano = {"i": 1, "v": 5, "x": 10, "l": 50, "c":100, "d":500, "m":1000}
+numero=input("Ingrese un numero romano a convertir\n")
+def romano_to_decimal(numero_romano):
+    if len(numero_romano) == 1:
+        return romano[numero_romano]
+    else:
+        if romano[numero_romano[0]] >= romano[numero_romano[1]]:
+            return romano[numero_romano[0]] + romano_to_decimal(numero_romano[1:])
+        else:
+            return - romano[numero_romano[0]] + romano_to_decimal(numero_romano[1:])
+
+print(romano_to_decimal(numero))
+
+
+#Ejercicio 22
+cantidad_objetos_sacados=0
+mochila1=['arroz ','pan' ,'sable de luz' ]
+def usar_la_fuerza(mochila,pos):
+    
+    if len(mochila)==0:
+        return 'No hay mas objetos en la mochila'
+    else :
+        if mochila[0]=='Sable de luz' :
+                     return print('El sable de luz se encontro en ', pos= pos+1)
+        else:                    
+            return usar_la_fuerza(mochila.pop(0),pos= pos+1)
+
+print(usar_la_fuerza(mochila1,cantidad_objetos_sacados))        
+
+
